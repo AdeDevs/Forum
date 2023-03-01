6fc5aecfc019a8bfbc9ab88244b914ab2536ee7a
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-01 23:53:11 +0100

    Updated

---
+++ src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Forum Home Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ForumHomePage",
+    props: {
+        msg: String
+    }
+}
+</script>
